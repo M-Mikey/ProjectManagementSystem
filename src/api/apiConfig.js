94ea57842c3de3720e69bba6b4e76882b337a787
Mohit Sharma <mohitsharma8700758407@gmail.com>
2026-03-23 10:55:29 +0530

    Update API config

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 // export const API_URL = "https://localhost:7255";
 
-export const API_URL = "https://procedures-agrees-fisher-bowl.trycloudflare.com";
+export const API_URL = "https://wilderness-reward-ashley-fairly.trycloudflare.com";
 
 export const API_BASE_URL_2 = "https://localhost:5001/api";
 
