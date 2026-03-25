1b2309c93e32493701b8ab92d3eceb7d6b58f333
Mohit Sharma <mohitsharma8700758407@gmail.com>
2026-03-25 10:11:49 +0530

    appconfig update

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
  //export const API_URL = "https://localhost:7255";
 
- export const API_URL = "https://favor-regarded-damaged-parker.trycloudflare.com";
+ export const API_URL = "https://findlaw-voters-publication-seniors.trycloudflare.com";
 
 export const API_BASE_URL_2 = "https://localhost:5001/api";
 
